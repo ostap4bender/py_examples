c11e010bf3d290c8fc1b84501cea69b3862ce86e
ostap4bender <olimputki@yandex.ru>
2020-11-29 12:12:09 +0300

    -> Простые делители да еще и со стрелочкой <-

--- 1.py
+++ 1.py
@@ -19,3 +19,16 @@ colors = (['green', 'red  ', 'green', 'yellow',
 for _ in range(10): print(green_metro_branch(random.choice(colors)))
 
 
+print('Ура, я = MAIN')
+print('__CODE_FOR_GET_PRIME_DIVIDERS__')
+
+
+def get_div(a, div):
+    for i in range(2, int(a**0.5)+1):
+        if a % i == 0: return get_div(a / i, div + [i])
+    return div + [int(a)]
+
+
+for i in range(1, 10**4):
+    print(i, get_div(i, []), sep=' -> ')
+
